84679e683bd5b2a30e305bc18d46a0e429250e2e
Prajen Sk <123826741+prajen22@users.noreply.github.com>
2025-02-17 08:51:50 +0530

    Update Background.py

--- Background.py
+++ Background.py
@@ -2,9 +2,9 @@ import base64
 import os
 
 class BackgroundCSSGenerator:
-    def __init__(self, img1_path, img2_path):
+    def __init__(self, img1_path):
         self.img1_path = img1_path
-        self.img2_path = img2_path
+        
 
     def get_img_as_base64(self, file):
         # Ensure the file exists before opening
@@ -17,9 +17,9 @@ class BackgroundCSSGenerator:
 
     def generate_background_css(self):
         img1 = self.get_img_as_base64(self.img1_path)
-        img2 = self.get_img_as_base64(self.img2_path)
+        
 
-        if not img1 or not img2:
+        if not img1 or:
             return "<style></style>"  # Return empty CSS if images are missing
 
         css = f"""
@@ -32,12 +32,7 @@ class BackgroundCSSGenerator:
         }}
 
         /* Apply background to the sidebar */
-        [data-testid="stSidebarContent"] {{
-            background-image: url("data:image/gif;base64,{img2}");
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-        }}
+        
 
         /* Transparent header */
         [data-testid="stHeader"] {{
